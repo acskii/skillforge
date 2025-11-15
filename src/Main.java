6fac41742e8e8aa8469bafbae7222b755324198c
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-11-15 15:47:49 +0200

    login and signup pages

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import pages.LoginPage;
+import pages.SignupPage;
 import windows.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
         /* Pages */
-        // add pages here
+        MainWindow.addPage("login", new LoginPage());
+        MainWindow.addPage("signup", new SignupPage());
 
         /* Starting up main window */
         MainWindow.nameWindow("SkillForge");
         MainWindow.addIcon("src/resources/icon.png");
         // If resizing the window is needed, uncomment the line below
         // MainWindow.setDimensions(800, 600);
+        MainWindow.goTo("login");
         MainWindow.start();
     }
 }
